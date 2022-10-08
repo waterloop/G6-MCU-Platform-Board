0ef1d8233fc06f1a8fad22a96bbc713db68390ab
Quinn Hodges <qhodges0442@gmail.com>
2022-10-08 19:24:34 -0400

    Clean out dead code

--- STM32G473CBTx/Core/Src/main.cpp
+++ STM32G473CBTx/Core/Src/main.cpp
@@ -195,49 +195,6 @@ void SystemClock_Config(void)
   }
 }
 
-// /**
-//   * @brief FDCAN1 Initialization Function
-//   * @param None
-//   * @retval None
-//   */
-// static void MX_FDCAN1_Init(void)
-// {
-
-//   /* USER CODE BEGIN FDCAN1_Init 0 */
-
-//   /* USER CODE END FDCAN1_Init 0 */
-
-//   /* USER CODE BEGIN FDCAN1_Init 1 */
-
-//   /* USER CODE END FDCAN1_Init 1 */
-//   hfdcan1.Instance = FDCAN1;
-//   hfdcan1.Init.ClockDivider = FDCAN_CLOCK_DIV1;
-//   hfdcan1.Init.FrameFormat = FDCAN_FRAME_CLASSIC;
-//   hfdcan1.Init.Mode = FDCAN_MODE_INTERNAL_LOOPBACK;
-//   hfdcan1.Init.AutoRetransmission = DISABLE;
-//   hfdcan1.Init.TransmitPause = DISABLE;
-//   hfdcan1.Init.ProtocolException = DISABLE;
-//   hfdcan1.Init.NominalPrescaler = 1;
-//   hfdcan1.Init.NominalSyncJumpWidth = 1;
-//   hfdcan1.Init.NominalTimeSeg1 = 2;
-//   hfdcan1.Init.NominalTimeSeg2 = 2;
-//   hfdcan1.Init.DataPrescaler = 1;
-//   hfdcan1.Init.DataSyncJumpWidth = 1;
-//   hfdcan1.Init.DataTimeSeg1 = 1;
-//   hfdcan1.Init.DataTimeSeg2 = 1;
-//   hfdcan1.Init.StdFiltersNbr = 0;
-//   hfdcan1.Init.ExtFiltersNbr = 0;
-//   hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
-//   if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
-//   {
-//     Error_Handler();
-//   }
-//   /* USER CODE BEGIN FDCAN1_Init 2 */
-
-//   /* USER CODE END FDCAN1_Init 2 */
-
-// }
-
 /**
   * @brief I2C1 Initialization Function
   * @param None
